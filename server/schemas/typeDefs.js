3c5f6f7c243979de62afd00aebfdf6d810423af7
Arturo Arce <arturo.arce@Arturos-MacBook-Pro.local>
2022-11-03 22:33:31 -0400

    updated schema and add mutation type

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -28,6 +28,13 @@ const typeDefs = gql`
   type Query {
     users: User
   }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: BookInput): User
+    removeBook(bookId: String!): User
+  }
 `;
 
 // export the typeDefs
