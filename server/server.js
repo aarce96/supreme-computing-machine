567f033a7715689f051d68da00c32e3ee7c12d33
Arturo Arce <arturo.arce@Arturos-MacBook-Pro.local>
2022-11-03 23:29:37 -0400

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,37 +1,41 @@
-const express = require('express');
+const express = require("express");
 // import ApolloServer
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer } = require("apollo-server-express");
 
 // import our typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const { typeDefs, resolvers } = require("./schemas");
+
+// import authentication
+const { authMiddleware } = require("./utils/auth");
+
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
-// create a new Apollo server and pass in our schema data
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-});
-
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Create a new instance of an Apollo server with the GraphQL schema
-const startApolloServer = async (typeDefs, resolvers) => {
-await server.start();
-// integrate our Apollo server with the Express application as middleware
-server.applyMiddleware({ app });
-
-db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      // log where we can go to test our GQL API
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    })
-  })
+const startApolloServer = async () => {
+  // create a new Apollo server and pass in our schema data
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  // integrate our Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
+  // log where we can go to test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
 
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
+
 // Call the async function to start the server
-startApolloServer(typeDefs, resolvers);
+startApolloServer();
