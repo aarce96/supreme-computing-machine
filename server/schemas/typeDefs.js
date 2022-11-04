e4e936ac2aeca0d99aad188839a3f2288f8fb4e6
Arturo Arce <arturo.arce@Arturos-MacBook-Pro.local>
2022-11-03 22:30:14 -0400

    updated schemas

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,11 +1,33 @@
 // import the gql tagged template function
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 // create our typeDefs
 const typeDefs = gql`
-    type Query {
-        helloWorld: String
-    }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: User
+  }
 `;
 
 // export the typeDefs
