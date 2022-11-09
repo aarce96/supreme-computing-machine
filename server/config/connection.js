59b30ddc6dd7eb7979b649563bdf2d7698d091d3
Arturo Arce <arturo.arce@Arturos-MBP.lan1>
2022-11-08 21:40:37 -0500

    update connection.js with localhost googlebooks

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/supreme-computing-machine', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
